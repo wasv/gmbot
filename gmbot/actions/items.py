f9cfa02d0cd608c90395bfed715defc5ea8d20e0
William Stevens <contact@wastevensv.com>
2017-07-26 19:14:30 -0400

    Added inventory commands.

---
+++ gmbot/actions/items.py
@@ -0,0 +1,86 @@
+inv_stat = 'items'
+class Inventory(object):
+    def run(self, context, state, args):
+        chan = context['chan']
+        if len(args) > 0:
+            user = args[0]
+        else:
+            user = context['user']
+
+        try:
+            stats = state.get(chan, user)
+        except KeyError:
+            return "Unknown user"
+
+        if 'items' in stats:
+            result = stats['items']
+            if result == []:
+                return "%s has nothing in their inventory" % (user)
+            return "%s has %s in their inventory" % (user, result)
+        else:
+            return "No inventory data for %s" % (user)
+
+    def help(self):
+        return """Lists a players inventory.
+    Usage: [user] (can be ommitted to check your inventory)"""
+
+class InventoryAdd(object):
+    def run(self, context, state, args):
+        chan = context['chan']
+        if len(args) > 1:
+            user = args[0]
+            item = args[1]
+        else:
+            user = context['user']
+            item = args[0]
+
+        try:
+            stats = state.get(chan, user)
+        except KeyError:
+            stats = {}
+
+        if 'items' in stats:
+            result = stats['items']
+        else:
+            result = []
+
+        result.append(item)
+        state.set(chan, user, 'items', result)
+        return "%s has added %s to their inventory" % (user, item)
+
+    def help(self):
+        return """Adds an item to a players inventory.
+    Usage: [user] [item] ([user] can be ommitted to check your inventory)"""
+
+class InventoryRemove(object):
+    def run(self, context, state, args):
+        chan = context['chan']
+        if len(args) > 1:
+            user = args[0]
+            item = args[1]
+        else:
+            user = context['user']
+            item = args[0]
+
+        try:
+            stats = state.get(chan, user)
+        except KeyError:
+            stats = {}
+
+        if 'items' in stats:
+            result = stats['items']
+        else:
+            result = []
+
+        if item in result:
+            result.remove(item)
+        else:
+            return "%s does not have a %s." % (user, item)
+
+        state.set(chan, user, 'items', result)
+
+        return "%s has removed %s from their inventory" % (user, item)
+
+    def help(self):
+        return """Removes an item from a players inventory.
+    Usage: [user] [item] ([user] can be ommitted to check your inventory)"""
--- gmbot/dispatcher.py
+++ gmbot/dispatcher.py
@@ -1,16 +1,21 @@
-from gmbot.actions.roll import Roll
-from gmbot.actions.stats import *
+import gmbot.actions.roll as roll
+import gmbot.actions.stats as stats
+import gmbot.actions.items as items
 from gmbot.jsonstore import JsonStore
+import shlex
 
 class Dispatcher(object):
     state = JsonStore() # Create persistent data store
     action_map = { # Mapping of command words to actions.
-        "roll": Roll,
-        "stat": StatCheck,
-        "statget": StatCheck,
-        "statcheck":StatCheck,
-        "statset": StatSet,
-        "statmod": StatMod,
+        "roll": roll.Roll,
+        "stat": stats.StatCheck,
+        "statget": stats.StatCheck,
+        "statcheck":stats.StatCheck,
+        "statset": stats.StatSet,
+        "statmod": stats.StatMod,
+        "inventory": items.Inventory,
+        "obtain": items.InventoryAdd,
+        "drop": items.InventoryRemove,
         }
 
     def dispatch(self, chan, user, message):
@@ -22,12 +27,12 @@ class Dispatcher(object):
             message(String: The message that was sent.
         Returns: (String) Result of command.
         """
-        args = message.split(' ')
+        args = shlex.split(message)
         if args[0] == "help": # Check for help keyword.
             if len(args) > 1:
                 action = args[1]
             else:
-                return "Commands:"+str([k for k in self.action_map.keys()])
+                return "Commands:"+str([k for k in sorted(self.action_map.keys())])
             if action in self.action_map:
                 return self.action_map[action]().help()
             else:
